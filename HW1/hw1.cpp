1ca67131c4b53a7cab3dc022ae9f1bcd0fce3ae6
lawlietye <jimingye@gmail.com>
2015-09-15 18:08:01 -0400

    09/15/2015
    used for loop to initialize in main function~

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -152,74 +152,28 @@ PrepaidAccount AccountStore::getAccount(int i)
 
 int main()
 {	
-	AccountStore AS;
-	stringstream strstream0, strstream1, strstream2, strstream3, strstream4;
-	srand(time(NULL));
-	long m0 = rand() % 9000000000 + 1000000000;
-	long m1 = rand() % 9000000000 + 1000000000;
-	long m2 = rand() % 9000000000 + 1000000000;
-	long m3 = rand() % 9000000000 + 1000000000;
-	long m4 = rand() % 9000000000 + 1000000000;
-	strstream0 << m0;
-	strstream1 << m1;
-	strstream2 << m2;
-	strstream3 << m3;
-	strstream4 << m4;
-	string s0 = strstream0.str();
-	string s1 = strstream1.str();
-	string s2 = strstream2.str();
-	string s3 = strstream3.str();
-	string s4 = strstream4.str();
-	double am0 = rand() % 50 + 51;
-	double am1 = rand() % 50 + 51;
-	double am2 = rand() % 50 + 51;
-	double am3 = rand() % 50 + 51;
-	double am4 = rand() % 50 + 51;
-	int sec0 = rand() % 190 + 11;
-	int sec1 = rand() % 190 + 11;
-	int sec2 = rand() % 190 + 11;
-	int sec3 = rand() % 190 + 11;
-	int sec4 = rand() % 190 + 11;
-	double cost0 = (rand() % 5 + 1)/10.0;
-	double cost1 = (rand() % 5 + 1)/10.0;
-	double cost2 = (rand() % 5 + 1)/10.0;
-	double cost3 = (rand() % 5 + 1)/10.0;
-	double cost4 = (rand() % 5 + 1)/10.0;
-	PrepaidAccount a0(s0, am0);
-	PrepaidAccount a1(s1, am1);
-	PrepaidAccount a2(s2, am2);
-	PrepaidAccount a3(s3, am3);
-	PrepaidAccount a4(s4, am4);
-	a0.PayForCall(sec0, cost0);
-	a1.PayForCall(sec1, cost1);
-	a2.PayForCall(sec2, cost2);
-	a3.PayForCall(sec3, cost3);
-	a4.PayForCall(sec4, cost4);
-	//cout << s0 << ' ' << am0 << ' ' <<  a0.PayForCall(sec0, cost0) << ' ' << a0.availablebalance() << endl;
-	//cout << s1 << ' ' << am1 << ' ' <<  a1.PayForCall(sec1, cost1) << ' ' << a1.availablebalance() << endl;
-	//cout << s2 << ' ' << am2 << ' ' <<  a2.PayForCall(sec2, cost2) << ' ' << a2.availablebalance() << endl;
-	//cout << s3 << ' ' << am3 << ' ' <<  a3.PayForCall(sec3, cost3) << ' ' << a3.availablebalance() << endl;
-	//cout << s4 << ' ' << am4 << ' ' <<  a4.PayForCall(sec4, cost4) << ' ' << a4.availablebalance() << endl;
 	int count = 0;
-	AS.insert(a0);
-	++count;
-	AS.insert(a1);
-	++count;
-	AS.insert(a2);
-	++count;
-	AS.insert(a3);
-	++count;
-	AS.insert(a4);
-	++count;
-	//AS.remove(a2);
+	AccountStore AS;
+	stringstream strstream;
+	srand(time(NULL));
+	for(int n = 0; n < 5; ++n)
+	{
+		long m = rand() % 9000000000 + 1000000000;
+		strstream << m;
+		string s = strstream.str();
+		double am = rand() % 50 + 51;
+		int sec = rand() % 190 + 11;
+		double cost = (rand() % 5 + 1)/10.0;
+		PrepaidAccount a(s, am);
+		a.PayForCall(sec, cost);
+		AS.insert(a);
+		//AS.insert1(a);
+		strstream.str("");
+		++count;
+	}
+	//AS.sorting();
+	//AS.remove(AS.getAccount(2));
 	//--count;
-	//cout << AS.getSize() << endl;
-	/*AS.insert1(a0);
-	AS.insert1(a1);
-	AS.insert1(a2);
-	AS.insert1(a3);
-	AS.insert1(a4);
-	AS.sorting();*/
 	for(int i = 0; i < count; ++i)
 	{
 		cout << AS.getAccount(i + 1).getnumber() << ' ' << AS.getAccount(i + 1).availablebalance() << endl;
