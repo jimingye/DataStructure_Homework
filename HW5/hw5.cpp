3c14547b928ae4b09877e54acc68f6cd0b0c7074
lawlietye <jimingye@gmail.com>
2015-11-09 19:28:08 -0500

    11/09/2015
    finished.

--- HW5/hw5.cpp
+++ HW5/hw5.cpp
@@ -1,5 +1,5 @@
 /*
-Homework_4
+Homework_5
 Jiming Ye
 N16566270
 jy1769@nyu.edu
@@ -18,65 +18,65 @@ using std::endl;
 
 struct BTnode
 {
-	string data;
+	char data;
 	BTnode *left;
 	BTnode *right;
-	BTnode(string str): data(str), left(NULL), right(NULL) {}
+	BTnode() = default;
+	BTnode(char ch): data(ch), left(NULL), right(NULL) {}
 };
 
 class Binary_tree
 {
-public:
+public:                                                                                                //can call the root directly(Tree.root).
 	BTnode *root;
 
 public:
-	Binary_tree(){root = NULL}
+	Binary_tree(){root = new BTnode();}
+	~Binary_tree(){};
 
 	BTnode* create_tree(string s, int index);
+	void assign_preorder(BTnode *&root, string s, string::iterator &siter);
+	void assign_inorder(BTnode *&root, string s, string::iterator &siter);
+	void assign_postorder(BTnode *&root, string s, string::iterator &siter);
 	BTnode* call_assign_preorder(BTnode *&root, string s, int index);
 	BTnode* call_assign_inorder(BTnode *&root, string s, int index);
 	BTnode* call_assign_postorder(BTnode *&root, string s, int index);
-	void assign_preorder(BTnode *&root, string s, string::iterator siter);
-	void assign_inorder(BTnode *&root, string s, string::iterator siter);
-	void assign_postorder(BTnode *&root, string s, string::iterator siter);
 	void traversal_preorder(BTnode *root);
 	void traversal_inorder(BTnode *root);
 	void traversal_postorder(BTnode *root);
 };
 
-BTnode* Binary_tree::create_tree(string s, int index)
+BTnode* Binary_tree::create_tree(string s, int index)                                                  //create a complete binary tree.
 {
-	//int length;
-	//for(; siter <= s.end() - 1; siter = siter + 2)
-	//{
-	//	++length;
-	//}
 	if(s.begin() == s.end())
 		return NULL;
-	queue<BTnode*> BTqueue;
-	auto siter = s.begin() + index;
-	BTnode *root = new BTnode(*siter);
-	siter = siter + 2;
-	queue.push(root);
-	BTnode *p = NULL;
-	while(!queue.empty() && siter <= s.end() - 1)
+	else
 	{
-		p = queue.front();
-		queue.pop();
-		if(siter <= s.end() - 1)
-		{
-			BTnode *leftnode = new BTnode(*siter);
-			p -> left = leftnode;
-			queue.push(leftnode);
-		}
+		queue<BTnode*> BTqueue;
+		auto siter = s.begin() + index;
+		root -> data = *siter;
 		siter = siter + 2;
-		if(siter <= s.end() - 1)
+		BTqueue.push(root);
+		BTnode *p = NULL;
+		while(!BTqueue.empty())
 		{
-			BTnode *righttnode = new BTnode(*siter);
-			p -> right = rightnode;
-			queue.push(rightnode);
+			p = BTqueue.front();
+			BTqueue.pop();
+			if(siter <= s.end() - 1)
+			{
+				BTnode *leftnode = new BTnode(*siter);
+				p -> left = leftnode;
+				BTqueue.push(leftnode);
+				siter = siter + 2;
+			}
+			if(siter <= s.end() - 1)
+			{
+				BTnode *rightnode = new BTnode(*siter);
+				p -> right = rightnode;
+				BTqueue.push(rightnode);
+				siter = siter + 2;
+			}
 		}
-		siter = siter + 2;
 	}
 	return root;
 }
@@ -88,15 +88,15 @@ BTnode* Binary_tree::call_assign_preorder(BTnode *&root, string s, int index)
 	return root;
 }
 
-void Binary_tree::assign_preorder(BTnode *&root, string s, string::iterator siter)
+void Binary_tree::assign_preorder(BTnode *&root, string s, string::iterator &siter)                    //iterator is used by reference. Always change itself.
 {
-	if(siter > s.end())
-		root = NULL;
+	if(root == NULL && siter > s.end())                                                                //decide whether reaching the leaves or the last element in string.
+		return;
 	else 
 		{
-			root -> data = *siter;
-			assign_preorder(root -> left, s, siter = siter + 2 );
-			assign_preorder(root -> right, s, siter = siter + 2);
+			root -> data = *siter, siter = siter + 2;                                                  //assign the element to root, and skip ' '.
+			assign_preorder(root -> left, s, siter);
+			assign_preorder(root -> right, s, siter);
 		}
 }
 
@@ -107,15 +107,15 @@ BTnode* Binary_tree::call_assign_inorder(BTnode *&root, string s, int index)
 	return root;
 }
 
-void Binary_tree::assign_inorder(BTnode *&root, string s, string::iterator siter)
+void Binary_tree::assign_inorder(BTnode *&root, string s, string::iterator &siter)
 {
-	if(siter > s.end())
-		root = NULL;
+	if(root == NULL && siter > s.end())
+		return;
 	else 
 		{
-			assign_inorder(root -> left, s, siter = siter + 2 );
-			root -> data = *siter;
-			assign_inorder(root -> right, s, siter = siter + 2);
+			assign_inorder(root -> left, s, siter);
+			root -> data = *siter, siter = siter + 2;
+			assign_inorder(root -> right, s, siter);
 		}
 }
 
@@ -126,15 +126,15 @@ BTnode* Binary_tree::call_assign_postorder(BTnode *&root, string s, int index)
 	return root;
 }
 
-void Binary_tree::assign_postorder(BTnode *&root, string s, string::iterator siter)
+void Binary_tree::assign_postorder(BTnode *&root, string s, string::iterator &siter)
 {
-	if(siter > s.end())
-		root = NULL;
+	if(root == NULL && siter > s.end())
+		return;
 	else 
 		{
-			assign_postorder(root -> left, s, siter = siter + 2 );
-			assign_postorder(root -> right, s, siter = siter + 2);
-			root -> data = *siter;
+			assign_postorder(root -> left, s, siter);
+			assign_postorder(root -> right, s, siter);
+			root -> data = *siter, siter = siter + 2;
 		}
 }
 
@@ -181,15 +181,15 @@ int main()
 	cout << "Please enter the create command and numbers in the tree: " << endl;
 	getline(cin, s1);
 	auto iter_1 = s1.begin();
-	if(*(iter_1 + 3) == 'r' || *(iter_1 + 3) == 'R')
+	if(*(iter_1 + 3) == 'R' || *(iter_1 + 3) == 'r')
 	{
 		Tree.create_tree(s1, 6);
 	}
-	else if(*(iter_1 + 3) == 'o' || *(iter_1 + 3) == 'O')
+	else if(*(iter_1 + 3) == 'O' || *(iter_1 + 3) == 'o')
 	{
 		Tree.create_tree(s1, 7);
 	}
-	else if(*(iter_1 + 3) == 'n' || *(iter_1 + 3) == 'N')
+	else if(*(iter_1 + 3) == 'N' || *(iter_1 + 3) == 'n')
 	{
 		Tree.create_tree(s1, 5);
 	}
@@ -197,21 +197,21 @@ int main()
 	cout << "Please enter the print command: " << endl;
 	getline(cin, s2);
 	auto iter_2 = s2.begin();
-	if(*(iter_2 + 3) == 'r' || *(iter_2 + 3) == 'R')
+	if(*(iter_2 + 3) == 'R' || *(iter_2 + 3) == 'r')
 	{
 		Tree.call_assign_preorder(Tree.root, s1, 6);
 		cout << "Output: " << endl;
 		Tree.traversal_preorder(Tree.root);
 		cout << endl;
 	}
-	else if(*(iter_2 + 3) == 'o' || *(iter_2 + 3) == 'O')
+	else if(*(iter_2 + 3) == 'O' || *(iter_2 + 3) == 'o')
 	{
 		Tree.call_assign_postorder(Tree.root, s1, 7);
 		cout << "Output: " << endl;
 		Tree.traversal_postorder(Tree.root);
 		cout << endl;
 	}
-	else if(*(iter_2 + 3) == 'n' || *(iter_2 + 3) == 'N')
+	else if(*(iter_2 + 3) == 'N' || *(iter_2 + 3) == 'n')
 	{
 		Tree.call_assign_inorder(Tree.root, s1, 5);
 		cout << "Output: " << endl;
--- HW5/test.cpp
+++ HW5/test.cpp
@@ -1,27 +1,188 @@
 #include <iostream>
 #include <string>
+#include <queue>
 
-using namespace::std;
+using std::string;
+using std::queue;
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct BTnode
+{
+    char data;
+    BTnode *left;
+    BTnode *right;
+    BTnode() = default;
+    BTnode(char ch): data(ch), left(NULL), right(NULL) {}
+};
+
+class Binary_tree
+{
+public:
+    BTnode *root;
+    
+public:
+    Binary_tree(){root = new BTnode();}
+    ~Binary_tree(){}
+    
+    BTnode* create_tree(string s, int index);
+    BTnode*& get_root(){return root;};
+    void assign_preorder(BTnode *&root, string s, string::iterator &siter);
+    void assign_inorder(BTnode *&root, string s, string::iterator siter);
+    void assign_postorder(BTnode *&root, string s, string::iterator siter);
+    BTnode* call_assign_preorder(BTnode *&root, string s, int index);
+    BTnode* call_assign_inorder(BTnode *&root, string s, int index);
+    BTnode* call_assign_postorder(BTnode *&root, string s, int index);
+    void traversal_preorder(BTnode *root);
+    void traversal_inorder(BTnode *root);
+    void traversal_postorder(BTnode *root);
+};
+
+BTnode* Binary_tree::create_tree(string s, int index)
+{
+    //int length;
+    //for(; siter <= s.end() - 1; siter = siter + 2)
+    //{
+    //	++length;
+    //}
+    if(s.begin() == s.end())
+        return NULL;
+    else
+    {
+        queue<BTnode*> BTqueue;
+        auto siter = s.begin() + index;
+        root -> data = *siter;
+        //cout << *siter << endl;
+        //cout << root -> data << endl;
+        siter = siter + 2;
+        BTqueue.push(root);
+        BTnode *p = NULL;
+        while(!BTqueue.empty() /*&& siter <= s.end() - 1*/)
+        {
+            p = BTqueue.front();
+            cout << p -> data << endl;
+            BTqueue.pop();
+            if(siter <= s.end() - 1)
+            {
+                BTnode *leftnode = new BTnode(*siter);
+                //cout << *siter << endl;
+                //cout << leftnode -> data << endl;
+                p -> left = leftnode;
+                BTqueue.push(leftnode);
+                siter = siter + 2;
+            }
+            if(siter <= s.end() - 1)
+            {
+                BTnode *rightnode = new BTnode(*siter);
+                //cout << *siter << endl;
+                //cout << rightnode -> data << endl;
+                p -> right = rightnode;
+                BTqueue.push(rightnode);
+                siter = siter + 2;
+            }
+        }
+    }
+    return root;
+}
+
+BTnode* Binary_tree::call_assign_preorder(BTnode *&root, string s, int index)
+{
+    auto siter = s.begin() + index;
+    assign_preorder(root, s, siter);
+    return root;
+}
+
+void Binary_tree::assign_preorder(BTnode *&root, string s, string::iterator &siter)
+{
+    if(root == NULL && siter > s.end())
+        return;
+    else
+    {
+        root -> data = *siter, siter = siter + 2;
+        assign_preorder(root -> left, s, siter);
+        assign_preorder(root -> right, s, siter);
+    }
+}
+
+BTnode* Binary_tree::call_assign_inorder(BTnode *&root, string s, int index)
+{
+    auto siter = s.begin() + index;
+    assign_inorder(root, s, siter);
+    return root;
+}
+
+void Binary_tree::assign_inorder(BTnode *&root, string s, string::iterator siter)
+{
+    if(root == NULL || siter > s.end())
+        return;
+    else
+    {
+        assign_inorder(root -> left, s, siter = siter + 2 );
+        root -> data = *siter;
+        assign_inorder(root -> right, s, siter = siter + 2);
+    }
+}
+
+BTnode* Binary_tree::call_assign_postorder(BTnode *&root, string s, int index)
+{
+    auto siter = s.begin() + index;
+    assign_postorder(root, s, siter);
+    return root;
+}
+
+void Binary_tree::assign_postorder(BTnode *&root, string s, string::iterator siter)
+{
+    if(root == NULL || siter > s.end())
+        return;
+    else
+    {
+        assign_postorder(root -> left, s, siter = siter + 2 );
+        assign_postorder(root -> right, s, siter = siter + 2);
+        root -> data = *siter;
+    }
+}
+
+void Binary_tree::traversal_preorder(BTnode *root)
+{
+    if(root == NULL)
+        return;
+    cout << root -> data << " ";
+    traversal_preorder(root -> left);
+    traversal_preorder(root -> right);
+}
+
+void Binary_tree::traversal_inorder(BTnode *root)
+{
+    if(root == NULL)
+        return;
+    else
+    {
+        traversal_inorder(root -> left);
+        cout << root -> data << " ";
+        traversal_inorder(root -> right);
+    }
+}
+
+void Binary_tree::traversal_postorder(BTnode *root)
+{
+    if(root == NULL)
+        return;
+    else
+    {
+        traversal_postorder(root -> left);
+        traversal_postorder(root -> right);
+        cout << root -> data << " ";
+    }
+}
 
 int main()
 {
-	string s;
-	int n;
-	getline(cin, s);
-	auto siter = s.begin();
-	while(1)
-	if(siter > s.end())
-	{
-		cout << *(siter - 2) << endl;
-		break;
-	}
-	else 
-		siter = siter + 2;
-	//for(; siter <= s.end() - 1; siter = siter + 2)
-	//{
-	//	cout << *siter << endl;
-	//	++n;
-	//}
-	//cout << *siter << endl;
-	return 0;
+    string s;
+    getline(cin,s);
+    Binary_tree Tree;
+    Tree.create_tree(s, 0);
+    Tree.call_assign_preorder(Tree.root, s, 0);
+    Tree.traversal_preorder(Tree.root);
+    return 0;
 }
